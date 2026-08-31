b28d116b5eb16e865624644e093d4c6985e458b9
SomeFlyingThing <306498559+SomeFlyingThing@users.noreply.github.com>
2026-08-31 18:27:35 +0100

    added tests

--- src/main.rs
+++ src/main.rs
@@ -40,3 +40,36 @@ fn calloc(size: usize) -> *mut u8 {
         allocate(start, size).map_or(std::ptr::null_mut(), Block::start_of_mem)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::ptr::{self, copy_nonoverlapping, slice_from_raw_parts};
+
+    use super::*;
+
+    #[test]
+    fn test_alloc() {
+        let mem = alloc(40);
+
+        let mut read = [1u8; 40];
+
+        unsafe {
+            ptr::write_bytes(mem, 3, 40);
+
+            copy_nonoverlapping(mem, read.as_mut_ptr(), 40);
+        }
+
+        let result = [3u8; 40];
+
+        assert!(result == read);
+    }
+
+    #[test]
+    fn test_calloc() {
+        let mem = calloc(40);
+
+        let read: &[u8] = unsafe { std::slice::from_raw_parts(mem, 40) };
+
+        assert_eq!(read, &[0u8; 40]);
+    }
+}
